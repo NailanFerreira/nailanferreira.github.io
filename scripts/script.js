32d4379b79f2960ec9bfbba63451b598b8201261
Nailan Ferreira <nailan4052@gmail.com>
2018-04-18 20:21:06 -0300

    iniciando desenvolvimento das funcionalidades

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,23 @@
 //Formula IMC = peso / (altura * altura)
 
 
-var trPacientes = document.getElementsByClassName("paciente");
+var peso = 0;
+var altura = 0;
 
-console.log(trPacientes);
+//parametros validos tem de ser maior que zero
+var parametrosValidos = (altura > 0 && peso > 0);
 
+if(parametrosValidos){
+    console.log("PARAMETROS VALIDOS");
+}
+else{
+    if(peso <= 0 && altura <= 0){
+        alert("Peso e Altura devem ser maior que 0!");
+    }
+    else if(peso <= 0){
+        alert("Peso deve ser maior que 0!");
+    }
+    else if(altura <= 0){
+        alert("Altura deve ser maior que 0!");
+    }
+}
